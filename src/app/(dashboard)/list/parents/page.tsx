0e99035829a27ff5889dfbbd5661ee711d0d3015
Ezaz Ahmed <ezazrahul794@gmail.com>
2024-11-08 22:04:43 +0600

    'dynamic-button'

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -1,4 +1,5 @@
 // import FormModal from "@/components/FormModal";
+import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
@@ -58,16 +59,11 @@ const ParentsListPage = () => {
             <td className="hidden md:table-cell">{item.address}</td>
             <td>
                 <div className="flex items-center gap-2">
-                    <Link href={`/list/teachers/${item.id}`}>
-                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-red-500">
-                            <Image src="/view.png" alt="" width={16} height={16} />
-                        </button>
-                    </Link>
                     {role === "admin" && (
-                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-yellow-500">
-                            <Image src="/delete.png" alt="" width={16} height={16} />
-                        </button>
-                        // <FormModal table="teacher" type="delete" id={item.id} />
+                        <>
+                            <FormModal table="parent" type="update" data={item} />
+                            <FormModal table="parent" type="delete" id={item.id} />
+                        </>
                     )}
                 </div>
             </td>
@@ -89,9 +85,7 @@ const ParentsListPage = () => {
                             <Image src="/sort.png" alt="" width={14} height={14} />
                         </button>
                         {role === "admin" && (
-                            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-                                <Image src="/plus.png" alt="" width={14} height={14} />
-                            </button>
+                            <FormModal table="parent" type="create" />
                         )}
                     </div>
                 </div>
---
+++ src/components/FormModal.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import Image from "next/image";
+
+const FormModal = ({ table, type, data, id }: {
+    table:
+    | "teacher"
+    | "student"
+    | "parent"
+    | "subject"
+    | "class"
+    | "lesson"
+    | "exam"
+    | "assignment"
+    | "result"
+    | "attendance"
+    | "event"
+    | "announcement";
+    type: "create" | "update" | "delete";
+    data?: any;
+    id?: number;
+}) => {
+    const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
+    const bgColor =
+        type === "create"
+            ? "bg-yellow-500"
+            : type === "update"
+                ? "bg-blue-500"
+                : "bg-purple-500";
+
+    return (
+        <>
+            <button
+                className={`${size} flex items-center justify-center rounded-full ${bgColor}`}
+            >
+                <Image src={`/${type}.png`} alt="" width={16} height={16} />
+            </button>
+        </>
+    )
+}
+
+export default FormModal
